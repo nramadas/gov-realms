1a24b95f1945d0ba53c885a07fb4fedb23f612ae
Niranjan Ramadas <mail@niranjan.me>
2022-06-10 18:17:28 -0400

    checkpoint

--- packages/ui/react/src/utils/styles/common/button.ts
+++ packages/ui/react/src/utils/styles/common/button.ts
@@ -1,19 +1,19 @@
 import { cx } from '../cx';
 
 export const BASE_STYLES = cx(
+  'rui-cursor-pointer',
   'rui-duration-300',
-  'rui-flex',
   'rui-font-serif',
   'rui-group',
+  'rui-inline-flex',
   'rui-items-center',
+  'rui-justify-center',
+  'rui-outline-0',
   'rui-relative',
   'rui-rounded-full',
   'rui-text-base',
   'rui-transition-all',
-  'disabled:rui-bg-white/40',
-  'disabled:rui-text-black',
-  'focus:rui-opacity-100',
-  'hover:rui-cursor-pointer'
+  'disabled:rui-cursor-not-allowed'
 );
 
 export const GRADIENT_UNPADDED = cx(
@@ -22,8 +22,6 @@ export const GRADIENT_UNPADDED = cx(
   'rui-bg-gradient-to-r',
   'rui-from-[#00C2FF]',
   'rui-opacity-[.84]',
-  'rui-px-11',
-  'rui-py-5',
   'rui-text-black',
   'rui-to-[#87F2FF]',
   'rui-transition-to-white-background',
@@ -32,12 +30,13 @@ export const GRADIENT_UNPADDED = cx(
   'hover:rui-opacity-100'
 );
 
-export const GRADIENT = cx(GRADIENT_UNPADDED, 'px-11', 'py-5');
+export const GRADIENT = cx(GRADIENT_UNPADDED, 'rui-px-11', 'rui-py-5');
 
 export const DARK_UNPADDED = cx(
   BASE_STYLES,
   'rui-bg-black',
   'rui-opacity-[.84]',
+  'rui-text-white',
   'active:rui-opacity-70',
   'focus:rui-bg-white',
   'focus:rui-text-black',
@@ -45,19 +44,23 @@ export const DARK_UNPADDED = cx(
   'hover:rui-text-black'
 );
 
-export const DARK = cx(DARK_UNPADDED, 'px-11', 'py-5');
+export const DARK = cx(DARK_UNPADDED, 'rui-px-11', 'rui-py-5');
 
 export const PRIMARY_UNPADDED = cx(
   BASE_STYLES,
   'rui-bg-white',
+  'rui-border-2',
+  'rui-border-transparent',
   'rui-text-black',
-  'rui-border-none',
   'active:rui-bg-white/50',
-  'hover:rui-bg-white/70',
-  'focus:rui-bg-white/70'
+  'disabled:rui-bg-white/10',
+  'disabled:active:rui-bg-white/10',
+  'disabled:focus:border-transparent',
+  'focus:rui-border-[#00C2FF]',
+  'hover:rui-bg-white/70'
 );
 
-export const PRIMARY = cx(PRIMARY_UNPADDED, 'px-11', 'py-5');
+export const PRIMARY = cx(PRIMARY_UNPADDED, 'rui-px-11', 'rui-py-5');
 
 export const SECONDARY_UNPADDED = cx(
   BASE_STYLES,
@@ -66,23 +69,36 @@ export const SECONDARY_UNPADDED = cx(
   'rui-text-white',
   'active:rui-bg-white/70',
   'active:rui-text-black',
-  'focus:rui-bg-white',
-  'focus:rui-text-black',
+  'disabled:rui-bg-white/10',
+  'disabled:rui-text-black',
+  'disabled:rui-cursor-not-allowed',
+  'disabled:rui-border-transparent',
+  'disabled:active:rui-bg-transparent',
+  'disabled:active:rui-text-white',
+  'disabled:focus:rui-border-transparent',
+  'disabled:hover:rui-bg-white/10',
+  'disabled:hover:rui-text-black',
+  'focus:rui-border-white',
   'hover:rui-bg-white',
   'hover:rui-text-black'
 );
 
-export const SECONDARY = cx(SECONDARY_UNPADDED, 'px-11', 'py-5');
+export const SECONDARY = cx(SECONDARY_UNPADDED, 'rui-px-11', 'rui-py-5');
 
 export const TERTIARY_WRAPPER_UNPADDED = cx(
   BASE_STYLES,
   'rui-group',
+  'rui-text-white',
   'active:rui-opacity-50',
+  'disabled:rui-opacity-30',
+  'disabled:active:rui-opacity-30',
+  'disabled:focus:rui-opacity-30',
+  'disabled:hover:rui-opacity-30',
   'focus:rui-opacity-[.70]',
   'hover:rui-opacity-[.70]'
 );
 
-export const TERTIARY_WRAPPER = cx(TERTIARY_WRAPPER_UNPADDED, 'px-11', 'py-5');
+export const TERTIARY_WRAPPER = cx(TERTIARY_WRAPPER_UNPADDED, 'rui-px-11', 'rui-py-5');
 
 export const TERTIARY = cx(
   'rui-border-b',
@@ -91,6 +107,8 @@ export const TERTIARY = cx(
   'rui-items-center',
   'rui-justify-center',
   'rui-transition',
+  'group-disabled:group-focus:rui-border-transparent',
+  'group-disabled:group-hover:rui-border-transparent',
   'group-focus:rui-border-white/70',
   'group-hover:rui-border-white/70'
 );
